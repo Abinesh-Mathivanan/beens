2842652d26160d02d317e170961e666caf47bd9e
Abinesh-Mathivanan <abineshmathivanan31@gmail.com>
2024-08-12 19:30:02 +0530

    deleted test

--- astro.config.ts
+++ astro.config.ts
@@ -2,17 +2,19 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import icon from "astro-icon";
+import icon from 'astro-icon';
 
-// No need to import cheerio explicitly, as it's already handled by astro-icon
-
-// (link unavailable)
 export default defineConfig({
   site: '(link unavailable)',
   vite: {
     ssr: {
-      // Exclude @iconify/tools and cheerio from external dependencies
       noExternal: ['@iconify/tools', 'cheerio'],
+    },
+    resolve: {
+      alias: {
+        // Alias to handle cheerio import
+        cheerio: require.resolve('cheerio/lib/cheerio.js'),
+      },
     },
   },
   integrations: [
